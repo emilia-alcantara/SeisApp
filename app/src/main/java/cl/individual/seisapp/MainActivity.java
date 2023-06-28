3783c3fa6e8e143bd9ed7d88aa10cd00efaa2f0c
Emilia <correoalcantaraemilia@gmail.com>
2023-06-27 20:15:23 -0400

    EJERCICIO 10: Se elimina el uso de findViewById, y se reemplaza por View Binding.

--- app/src/main/java/cl/individual/seisapp/MainActivity.java
+++ app/src/main/java/cl/individual/seisapp/MainActivity.java
@@ -8,35 +8,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import cl.individual.seisapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        EditText nombre = findViewById(R.id.editTextNombre);
-        EditText apellido = findViewById(R.id.editTextApellido);
-        EditText correo = findViewById(R.id.editTextEmail);
-        EditText contrasena = findViewById(R.id.editTextContrasena);
-
-
-        Button botonCrearCuenta = findViewById(R.id.botonCrearCuenta);
-
-        botonCrearCuenta.setOnClickListener(new View.OnClickListener() {
+        binding.botonCrearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = nombre.getText().toString();
-                String lastName = apellido.getText().toString();
-                String email = correo.getText().toString();
-                String password = contrasena.getText().toString();
+                String nombre = binding.editTextNombre.getText().toString();
+                String apellido = binding.editTextApellido.getText().toString();
+                String correo = binding.editTextEmail.getText().toString();
+                String clave = binding.editTextContrasena.getText().toString();
 
 
-                if (name.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || !email.contains("@")) {
+                if (nombre.isEmpty() || apellido.isEmpty() || correo.isEmpty() || clave.isEmpty() || !correo.contains("@")) {
                     Toast.makeText(getBaseContext(), "¡Debe completar todos los campos!", Toast.LENGTH_LONG).show();
                     return;
                 }
-                String textToast = "Nombre usuario: " + name + " " + lastName + " Correo Electronico: " + email + " Password : " + password;
+                String textToast = "Nombre usuario: " + nombre + " " + apellido + " Correo Electronico: " + correo + " Password : " + clave;
                 Toast.makeText(getBaseContext(), textToast, Toast.LENGTH_LONG).show();
             }
         });
